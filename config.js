39b08e55ca3f97a6db233ade16b3a72309cdeaa7
ICEFUZZYYY <jan.ludwig999@gmail.com>
2025-01-24 19:29:23 +0100

    updating gitignore for testing

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+    TWITCH_USER_ID: "1229411169",
+    TWITCH_CLIENT_ID: "0fj6rrjodzkpb2kdhjy7fr3js8u8ee",
+    TWITCH_ACCESS_TOKEN: "245sikwaoo5o38xegl16jagnrxvb7p"
+};
+
+export default config; 
