c167982d1039a76985e174545da91713b3e1c1c3
ICEFUZZYYY <jan.ludwig999@gmail.com>
2025-01-24 19:39:47 +0100

    UPDATING TOKEN

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     TWITCH_USER_ID: "1229411169",
     TWITCH_CLIENT_ID: "0fj6rrjodzkpb2kdhjy7fr3js8u8ee",
-    TWITCH_ACCESS_TOKEN: "245sikwaoo5o38xegl16jagnrxvb7p"
+    TWITCH_ACCESS_TOKEN: "st3f048wnwxqjwme2cndbqod54z7x9"
 };
 
 export default config; 
